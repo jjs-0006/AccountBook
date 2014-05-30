0e6f5dffba9c08672304d601306715d3e2cdc4b5
jjs-0006 <jjs-0006@outlook.jp>
2014-05-30 11:01:03 +0900

    ログイン処理を作成しました #29

--- src/jp/co/jjs/java_seminar/servlet/LoginQuery.java
+++ src/jp/co/jjs/java_seminar/servlet/LoginQuery.java
@@ -1,7 +1,7 @@
 package jp.co.jjs.java_seminar.servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,19 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import jp.co.jjs.java_seminar.dao.AccountDAO;
-import jp.co.jjs.java_seminar.javabeans.Data;
 
 /**
- * Servlet implementation class Statistics
+ * Servlet implementation class LoginQuery
  */
-@WebServlet("/statistics")
-public class Statistics extends HttpServlet {
+@WebServlet("/LoginQuery")
+public class LoginQuery extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Statistics() {
+    public LoginQuery() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,7 +34,7 @@ public class Statistics extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        process(request,response);
+        process(request, response);
     }
 
     /**
@@ -44,34 +43,29 @@ public class Statistics extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        process(request,response);
+        process(request, response);
     }
 
     private void process(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = request
-                .getRequestDispatcher("WEB-INF/statisticst.jsp");
+
+        String id = request.getParameter("id");
+        String pass = request.getParameter("pass");
         AccountDAO adao = new AccountDAO();
         HttpSession session = request.getSession();
-        int term = Integer.parseInt(request.getParameter("time"));
-        int type_number = Integer.parseInt(request.getParameter("grouping"));
-        int year = 2014;
-        int month = 5;
-        int user_number = 1;
-        int syear = 0;
-        int smonth = 0;
-        if((month - term)  > 0){
-            syear = year;
-            smonth = month - term;
+        RequestDispatcher dispatcher;
+        Calendar cal = Calendar.getInstance();
+        session.setAttribute("year", cal.get(Calendar.YEAR));
+        session.setAttribute("month", cal.get(Calendar.MONTH + 1));
+        int result = adao.login(id, pass);
+        if(result > 0){
+            session.setAttribute("user_number", result);
+            dispatcher = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
         }
         else{
-            syear = year - 1;
-            smonth = month - term + 12;
+            dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
         }
-        ArrayList<Data> datalist = adao.getTermData(syear, smonth, year, month, user_number, type_number);
-        session.setAttribute("datalist", datalist);
         dispatcher.forward(request, response);
 
     }
-
 }
